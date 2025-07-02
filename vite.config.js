c1b9e7da3ec37c8f4fa28295f3ad2a8190789c2e
OlgaBogdanova <OlgaBogdanova92yandex.ru>
2025-07-02 18:54:33 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "http://OlyaBogdanova.github.io/fs-test/",
+  base: "http://olyabogdanova.github.io/fs-test/",
   css: {
     preprocessorOptions: {
       scss: {
